0c95159e77e12edcd0fb12d6c2cfdcec4c839b4b
Cedric Lapi <lapicedric@gmail.com>
2024-02-14 22:32:23 +0100

    first commit

---
+++ first/sketch.js
@@ -0,0 +1,14 @@
+
+
+function setup() {
+    createCanvas(400, 400);
+    
+}
+
+function draw() {
+    background(220, 0, 200);
+    line(0, 0, 400, 400);
+    rectMode(CENTER);
+    rect(200, 200, 25, 75);
+    
+}
