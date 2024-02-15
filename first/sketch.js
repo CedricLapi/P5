74324d5cb7b362e8ac9bec75015bab47f96daf3f
Cedric Lapi <lapicedric@gmail.com>
2024-02-15 14:21:58 +0100

    more stuff added

--- first/sketch.js
+++ first/sketch.js
@@ -6,9 +6,16 @@ function setup() {
 }
 
 function draw() {
-    background(220, 0, 200);
-    line(0, 0, 400, 400);
+    background(100);
+    //line(0, 0, 400, 400);
     rectMode(CENTER);
-    rect(200, 200, 25, 75);
-    
+    //noFill();
+    fill(0,0,255);
+    stroke(0, 255, 0);
+    strokeWeight(3)
+    rect(200, 200, 150, 150);
+
+    fill(255, 0, 0, 175)
+    noStroke()
+    ellipse(160, 150, 100, 75)
 }
